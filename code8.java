ccf55bf6ab1087967e95a93bebc993992ffe22fa
prathibhakrishna133-create <prathibhakrishna133@gmail.com>
2026-01-29 15:32:23 +0530

    Add files via upload

---
+++ code8.java
@@ -0,0 +1,12 @@
+public class code8{
+    
+     public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=30;
+        double d=a*b*c/100;
+        {
+            System.out.println("Simple interest="+d);
+        }
+    }
+}
