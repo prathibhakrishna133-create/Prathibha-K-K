825f62efe7bdb73c5908efed5967e11a1a45cbe7
prathibhakrishna133-create <prathibhakrishna133@gmail.com>
2026-01-29 13:16:01 +0530

    Add files via upload

--- codetwo.java
+++ codetwo.java
@@ -1,15 +1,9 @@
-package day2;
-
-public class codetwo {
-public static void main(String[]args) {
-	int a=10;
-	int b=20;
-	int temp;
-	temp=a;
-	a=b;
-	b=temp;
-	System.out.println("after swapping");
-	System.out.println("a="+a);
-	System.out.println("b="+b);
-}
+public class codetwo{
+    public static void main(String[] args) {
+        int a=10;
+        double b;
+        b=a;
+        System.out.println(a);
+          System.out.println(b);
+    }
 }
