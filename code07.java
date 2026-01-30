0aa03c6bd7ff21d4d6cc08e4f179ef6d7d04233b
prathibhakrishna133-create <prathibhakrishna133@gmail.com>
2026-01-30 15:10:22 +0530

    Add files via upload

---
+++ code07.java
@@ -0,0 +1,13 @@
+class Car{
+    String color;
+    void run(){
+        System.out.println("running");
+    }
+}
+public class code07{
+    public static void main(String[] args) {
+        Car RR=new Car();
+    RR.color="red";
+    RR.run();    
+    }
+}
